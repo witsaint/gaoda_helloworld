ce554d85f21a2327c3e0bc340a9f01f74f3691f7
gaodingqiang <gaodingqiang@juxiangfen.com>
2017-08-17 16:50:39 +0800

    init spa

---
+++ src/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gaodingqiang on 2017/8/13.
+ */
+/* 引入vue和主页 */
+import Vue from 'vue'
+import router from './utils/router'
+import App from './App'
+import store from './utils/store'
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+/* 实例化一个vue */
+new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
+})
---
+++ src/utils/router.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 主入口
+import Index from './../views/index'
+import Home from './../views/home'
+import Who from './../views/who'
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Index,
+            meta: {requireAuth: true},
+            children: [
+                {
+                    path: 'home',
+                    component: Home,
+                    name: 'Home'
+                },  {
+                    path: 'who',
+                    component: Who,
+                    name: 'who'
+                }
+            ]
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(r => r.meta.requireAuth)) {
+        if (localStorage.getItem('token')) {
+            next()
+        } else {
+            // 提交store记住跳转前页面
+            // 防止重置密码流程重定向错误
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
+// 滚动回页面顶部处理
+router.afterEach((to, from, next) => {
+    window.scrollTo(0, 0)
+})
+
+export default router

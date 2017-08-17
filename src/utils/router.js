061f0ea28eac49a89a5ad09a6a8ff000d16a06aa
gaodingqiang <gaodingqiang@juxiangfen.com>
2017-08-17 20:14:37 +0800

    me

--- src/utils/router.js
+++ src/utils/router.js
@@ -14,7 +14,7 @@ const router = new Router({
         {
             path: '/',
             component: Index,
-            redirect: '/',
+            // redirect: '/',
             meta: {requireAuth: true},
             children: [
                 {
